1221e24432370d9c2d71f907bc67c0ce2f11fcd0
Woohyuk Song <harrywsong@naver.com>
2025-11-30 14:49:02 -0500

    created api.js to use for base api access in config folder

---
+++ frontend/src/config/api.js
@@ -0,0 +1,11 @@
+// base URL for API access
+export const API_BASE_URL = 'https://101524575-comp-3123-assignment1.vercel.app';
+
+// specific API endpoints
+export const API_ENDPOINTS = {
+  SIGNUP: '/api/v1/user/signup',
+  LOGIN: '/api/v1/user/login',
+  EMPLOYEES: '/api/v1/emp/employees',
+  EMPLOYEE_BY_ID: (id) => `/api/v1/emp/employees/${id}`,
+};
+
